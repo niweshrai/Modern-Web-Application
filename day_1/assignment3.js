46497ae4352a4c4f8dd5e58d304d626047aaa8f1
niwesh578 <ncrai@mum.edu>
2018-01-16 21:49:14 -0600

    Initial Commit

--- day_1/assignment3.js
+++ day_1/assignment3.js
@@ -1,8 +1,7 @@
 function isWeekend(){
-		const todayDate = new Date();
-    const day =  todayDate.getDay();
-    
-    return(day === 0 || day ===6) ? 'weekend' : 'weekday';
-}
-
+    const todayDate = new Date();
+    const day = todayDate.getDay();
+    let days = ['weekend', 'weekday', 'weekday', 'weekday', 'weekday', 'weekday', 'weekend']
+    return days[day];
+} 
 console.log(isWeekend());
